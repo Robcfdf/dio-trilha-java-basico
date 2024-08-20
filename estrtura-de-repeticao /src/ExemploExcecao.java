afaff11ea624dd5a44b4e0aa7c50656f47335d81
Robcfdf <eashenko@gmail.com>
2024-08-20 18:35:57 -0300

    atualização

--- estrtura-de-repeticao /src/ExemploExcecao.java
+++ estrtura-de-repeticao /src/ExemploExcecao.java
@@ -1,11 +1,7 @@
-import java.text.NumberFormat;
 
 public class ExemploExcecao {
-    ublic static void main(String[] args) {
+    public static void main(String[] args) {
         Number valor = Double.valueOf("a1.75");
-
-        
-        
         System.out.println(valor);
 }
 }
